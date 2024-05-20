90a31f232c41ef0181fe3d928f4661c812381839
Bart <barteqczech@gmail.com>
2024-05-20 21:52:33 +0200

    UI changes

--- app/characters/[id]/page.tsx
+++ app/characters/[id]/page.tsx
@@ -1,22 +1,8 @@
 import { fetchSpecificCharacter } from '@/app/lib/data';
 import { CharacterData } from '@/app/lib/definitions';
-// interface CharacterData {
-//   name: string;
-//   image: string;
-//   gender: string;
-//   location: {
-//     name: string;
-//   };
-//   origin: {
-//     name: string;
-//   };
-//   species: string;
-//   status: string;
-// }
 
 const fetchCharacter = async (id: string): Promise<CharacterData> => {
   const data = await fetchSpecificCharacter(id);
-
   return data;
 };
 
@@ -26,14 +12,28 @@ const CharacterPage = async ({ params }: { params: { id: string } }) => {
   const { name, image, gender, location, origin, species, status } = data;
 
   return (
-    <div className="max-w-lg mx-auto p-4 bg-white shadow-lg rounded-lg">
-      <h1 className="text-2xl font-bold mb-4">{name}</h1>
-      <img src={image} alt={name} className="w-full mb-4 rounded-lg" />
-      <p className="mb-2">Gender: {gender}</p>
-      <p className="mb-2">Location: {location.name}</p>
-      <p className="mb-2">Origin: {origin.name}</p>
-      <p className="mb-2">Species: {species}</p>
-      <p>Status: {status}</p>
+    <div className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg flex flex-col md:flex-row items-center">
+      <div className="w-full md:w-1/2 mb-6 md:mb-0 md:mr-6">
+        <img src={image} alt={name} className="w-full rounded-lg" />
+      </div>
+      <div className="w-full md:w-1/2 flex flex-col">
+        <h1 className="text-4xl font-bold mb-6">{name}</h1>
+        <p className="text-xl">
+          <span className="font-semibold">Gender:</span> {gender}
+        </p>
+        <p className="text-xl">
+          <span className="font-semibold">Location:</span> {location.name}
+        </p>
+        <p className="text-xl">
+          <span className="font-semibold">Origin:</span> {origin.name}
+        </p>
+        <p className="text-xl">
+          <span className="font-semibold">Species:</span> {species}
+        </p>
+        <p className="text-xl">
+          <span className="font-semibold">Status:</span> {status}
+        </p>
+      </div>
     </div>
   );
 };
